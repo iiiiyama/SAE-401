fbe312587162da99c1417ee202836fd342d42ce2
Baptiste Antony <bptstantony@gmail.com>
2023-06-07 16:29:32 +0200

    first commit

---
+++ banque/mybank/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class user_perso(AbstractUser):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=200)
+    email = models.EmailField(max_length=30)
+    num_compte = models.CharField(max_length=10, unique=True)
+    
+    
